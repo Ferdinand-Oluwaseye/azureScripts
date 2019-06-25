beb9fbf31c5ff4c2e6f7182029f9abcc2b6cd989
Oluwaseye Akinjise-Ferdinand <ronferd2012@gmail.com>
2019-06-25 16:38:58 +0100

    Moved executions to for loop

--- azureScripts/createMVM.sh
+++ azureScripts/createMVM.sh
@@ -1,19 +1,19 @@
 # create virtual machines using azure
 
 #create a resource group to be used
-az group create --name myresource --location uksouth
+#az group create --name myresource --location uksouth
 
 #create a virtual network and attach adress prefixes and a subnet
-az network vnet create --resource-group myresource --name myvirtualnetwork --address-prefixes 10.0.0.0/16 --subnet-name mysubnet --subnet-prefix 10.0.0.0/24
+#az network vnet create --resource-group myresource --name myvirtualnetwork --address-prefixes 10.0.0.0/16 --subnet-name mysubnet --subnet-prefix 10.0.0.0/24
 
 #create a network security group to manage port access to the virtual machine
-az network nsg create --resource-group myresource --name mynetworksecuritygroup
+#az network nsg create --resource-group myresource --name mynetworksecuritygroup
 
 #add rules to specify which ports are open
-az network nsg rule create --resource-group myresource --name SSH --priority 300 --nsg-name mynetworksecuritygroup --destination-port-ranges 22
+#az network nsg rule create --resource-group myresource --name SSH --priority 300 --nsg-name mynetworksecuritygroup --destination-port-ranges 22
 
 #create a public ip to access your virtual machine
-az network public-ip create --resource-group myresource --name mypublicip --dns-name ferdinand1234
+#az network public-ip create --resource-group myresource --name mypublicip --dns-name ferdinand1234
 
 #create the network interface card and attach the security group and public ip
 #az network nic create --resource-group myresource --name mynetworkinterface --vnet-name myvirtualnetwork --subnet mysubnet --network-security-group mynetworksecuritygroup --public-ip-address mypublicip
@@ -27,7 +27,12 @@ az network public-ip create --resource-group myresource --name mypublicip --dns-
 vms="jenkins jenkins-build python"
 
 for vm in ${vms}; do
-	az network nic create --resource-group myresource --name $vm-nics --vnet-name myvirtualnetwork --subnet mysubnet --network-security-group mynetworksecuritygroup --public-ip-address mypublicip
-	az vm create --resource-group myresource --name $vm --image UbuntuLTS --nics $vm-nics --admin-username "ferdinand" --size Standard_F1 --generate-ssh-keys
+	az group create --name $vm-resource --location uksouth
+	az network vnet create --resource-group $vm-resource --name $vm-virtualnetwork --address-prefixes 10.0.0.0/16 --subnet-name $vm-subnet --subnet-prefix 10.0.0.0/24
+	az network nsg create --resource-group $vm-resource --name $vm-nsg
+	az network nsg rule create --resource-group $vm-resource --name SSH --priority 300 --nsg-name $vm-nsg --destination-port-ranges 22
+	az network public-ip create --resource-group $vm-resource --name $vm-ip --dns-name $vm-ferdinand1234
+	az network nic create --resource-group $vm-resource --name $vm-nics --vnet-name $vm-virtualnetwork --subnet $vm-subnet --network-security-group $vm-nsg --public-ip-address $vm-ip
+	az vm create --resource-group $vm-resource --name $vm --image UbuntuLTS --nics $vm-nics --admin-username "ferdinand" --size Standard_F1 --generate-ssh-keys
 done
 
