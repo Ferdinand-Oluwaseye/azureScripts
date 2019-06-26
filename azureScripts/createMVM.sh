9a45f4e10fe5fc1351d386052bbc89ff584816a1
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-06-26 10:40:41 +0000

    change subnet name

--- azureScripts/createMVM.sh
+++ azureScripts/createMVM.sh
@@ -23,7 +23,7 @@
 #az vm create --resource-group myresource --name jenkins-Server --image UbuntuLTS --nics mynetworkinterface --admin-username "ferdinand" --size Standard_F1
 
 az group create --name myresourcegroup --location uksouth
-az network vnet create --resource-group myresourcegroup --name virtualnetwork --address-prefixes 10.0.0.0/16 --subnet-name $vm-subnet --subnet-prefix 10.0.0.0/24
+az network vnet create --resource-group myresourcegroup --name virtualnetwork --address-prefixes 10.0.0.0/16 --subnet-name mysubnet --subnet-prefix 10.0.0.0/24
 
 vms="jenkins jenkins-build python"
 
@@ -35,7 +35,7 @@ for vm in ${vms}; do
 	az network nsg rule create --resource-group myresourcegroup --name Jenkins-Server --priority 400 --nsg-name $vm-nsg --destination-port-ranges 8080
 	az network nsg rule create --resource-group myresourcegroup --name Python-Server --priority 500 --nsg-name $vm-nsg --destination-port-ranges 8000
 	az network public-ip create --resource-group myresourcegroup --name $vm-ip --dns-name $vm-ferdinand1234
-	az network nic create --resource-group myresourcegroup --name $vm-nics --vnet-name virtualnetwork --subnet $vm-subnet --network-security-group $vm-nsg --public-ip-address $vm-ip
+	az network nic create --resource-group myresourcegroup --name $vm-nics --vnet-name virtualnetwork --subnet mysubnet --network-security-group $vm-nsg --public-ip-address $vm-ip
 	az vm create --resource-group myresourcegroup --name $vm --image UbuntuLTS --nics $vm-nics --admin-username "ferdinand" --size Standard_F1 --generate-ssh-keys
 done
 
